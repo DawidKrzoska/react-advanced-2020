0c3c7dd30e9362ed70a633de20af278920249594
DawidKrzoska <dawidwilk19999@gmai.com>
2021-02-01 21:13:01 +0100

    Start multi return

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,26 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 // by default runs after every re-render
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0);
+  useEffect(() => {
+    if (value !== 0) {
+      console.log(value);
+      let title = "new title " + value;
+      document.title = title;
+    }
+  });
+
+  return (
+    <>
+      <h2>useEffect Basics</h2>
+      <button className="btn" onClick={() => setValue(value + 1)}>
+        {" "}
+        change title to {value}
+      </button>
+    </>
+  );
 };
 
 export default UseEffectBasics;
